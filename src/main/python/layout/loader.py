fdfa77b17252189eab325c7af801a6719abd3787
Bassem <bassem.magdyy94@gmail.com>
2019-11-23 21:38:38 +0200

    adding load data script and datfile

--- src/main/python/layout/loader.py
+++ src/main/python/layout/loader.py
@@ -1,9 +1,23 @@
 from PyQt5 import QtCore, QtWidgets, uic
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+                             QHBoxLayout, QVBoxLayout, QApplication, QMainWindow,  QInputDialog, QLineEdit, QFileDialog)
+from PyQt5.QtGui import QIcon
 
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon
+import pandas as pd
+import numpy as np
+# import matplotlib
+# import matplotlib.pyplot as plt
+import regex as re
 
 class LoadUIWindow(QtWidgets.QWidget):
     switch_window = QtCore.pyqtSignal(list)
-
+    # headers = []
+    # units = []
+    # info=[]
     def __init__(self, ui_file: str):
         super(LoadUIWindow, self).__init__()
         uic.loadUi(ui_file, self)
@@ -13,7 +27,7 @@ class LoadUIWindow(QtWidgets.QWidget):
         self.setFixedSize(471, 280)
 
         self.btn_open_file = self.findChild(QtWidgets.QPushButton, 'btnReadFile')
-        self.btn_open_file.clicked.connect(self.switcher)
+        self.btn_open_file.clicked.connect(self.saveFileDialog)
 
         self.btn_nswr = self.findChild(QtWidgets.QPushButton, 'btn_nswr')
         self.btn_nswr.clicked.connect(self.switcher)
@@ -45,3 +59,26 @@ class LoadUIWindow(QtWidgets.QWidget):
             "Moustafa",
         ]
         self.switch_window.emit(data)
+
+    
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(
+            self, "QFileDialog.getSaveFileName()", "", "All Files (*);;Text Files (*.txt)", options=options)
+        if fileName:
+            #read headers
+            df_headers = pd.read_csv(fileName, skiprows=18,nrows=1, header=None)
+            rx = r'(?V1)(?<=[^A-Za-z0-9])(?=\d)'
+
+            for index in range(df_headers.shape[1]):
+                columnSeriesObj = df_headers.iloc[: , index]
+                arr_headers = re.split(rx, ''.join(str(e) for e in columnSeriesObj.values[0]))
+                header_result = " ".join(arr_headers[0].split()).split(' ')
+            
+            #read data
+            df = pd.read_csv(fileName, sep='', header=None, names=header_result,
+                delimiter=r'\s+', low_memory=False, skiprows=22)
+
+            # replace unneeded values              
+            df.replace([65535, -999.250], np.nan, inplace=True)
